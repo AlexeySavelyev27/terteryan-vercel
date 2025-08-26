55c063c952d17245bb76cf53c4028e170a9058fc
AlexeySavelyev27 <promixnaugust@gmail.com>
2025-08-26 10:04:08 +0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+// Utility function for combining class names
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 // Utility functions for the website
 
 export function toggleTheme(isDark: boolean): void {
